fcdb49db2d35d562f98f4676cad7491b50b74aa0
Martin N <avgp_2k@yahoo.de>
2014-03-24 22:29:08 +0100

    Bye bye mkkernel, hello Makefile

--- mkkernel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir bin
-nasm boot/bootloader.asm -f bin -o bin/bootloader.bin
-gcc -ffreestanding -c kernel.c -o bin/kernel.o
-
-ld -Ttext 0x1000 --oformat binary -o bin/kernel.bin bin/kernel.o
-cat bin/bootloader.bin bin/kernel.bin > kernel.img
