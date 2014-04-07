6b85fe0ee7a669bde3e483c1547d02612cfdc6aa
Martin Naumann <martin.naumann@centralway.com>
2014-04-07 15:47:47 +0200

    Fixing pointer types in memset

--- kernel/memory.c
+++ kernel/memory.c
@@ -1,7 +1,7 @@
 #include "memory.h"
 
 void memset(void *addr, unsigned char value, unsigned int len) {
-    unsigned int end_addr = addr + len;
+    char *end_addr = addr + len;
     char *tmp = (char *)addr;
     while(tmp < end_addr) {
         *tmp = value;
